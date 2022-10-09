8b9655ea0fba2cf19321843e71155fc1d626eb3c
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-09 19:32:26 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  resources :houses
-  resources :apartments
-  resources :tenants
-  resources :landlords
+
+  resources :landlords,  only: [:index, :show, :create, :update, :destroy]
+
+  resources :apartments, only: [:index, :show, :create, :update, :destroy]
+
+  resources :tenants, only: [:index, :show, :create, :update, :destroy]
+
+  resources :houses, only: [:index, :show, :create, :update, :destroy]
+
+  
+
+ 
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
